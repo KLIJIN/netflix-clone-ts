a7c46fd075b4b74f9bb12234e39fd12675b1c7dc
KLIJIN <mdu091008@yandex.ru>
2021-06-19 04:37:45 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,11 @@ const App: FC = () => {
         <Banner />
         <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow={true} />
         <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-        {/* <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} /> */}
-        {/* <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} /> */}
-        {/* <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} /> */}
-        {/* <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-        <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} /> */}
+        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+        <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
       </div>
     </Router>
   );
